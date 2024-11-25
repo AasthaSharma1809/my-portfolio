80537dff35f42ec077f0867331adce2194336fbf
AasthaSharma1809 <aash22cse@cmrit.ac.in>
2024-11-25 13:53:46 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-window.onload = function() {
-    const header = document.getElementById("main-header");
-
-    // Check if the current page is not 'index.html'
-    if (window.location.pathname !== '/index.html' && header) {
-        header.style.display = 'none'; // Hide the header
-    }
-};
